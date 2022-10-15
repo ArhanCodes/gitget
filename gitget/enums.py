79f73740e3e5a8ca3f407199fdb412a7f40cd6fc
Arhan <106308047+ArhanCodes@users.noreply.github.com>
2022-10-15 09:12:16 +0400

    Create enums.py

---
+++ gitget/enums.py
@@ -0,0 +1,11 @@
+
+from enum import Enum
+
+
+class Emojis(Enum):
+    GITHUB = "<:GitHub:1000278943343063050>"
+    BULLET_POINT = "<:bulletPoint:987974786401579038>"
+
+
+class Colors(Enum):
+    DARK_MODE_BLEND = 0x2f3136
