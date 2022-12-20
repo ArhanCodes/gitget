bae84db092d0e3f06ddd97b4a5d8928e68dcabc1
Arhan <arhanh1234@gmail.com>
2022-12-20 11:39:23 +0400

    Update __main__.py

--- gitget/__main__.py
+++ gitget/__main__.py
@@ -7,35 +7,17 @@ from disnake.ext.commands import InteractionBot
 
 # Core handles registering commands, sync_commands should not be used.
 
-proxy_on = os.environ.get("REST_PROXY") is not None
-
-if proxy_on:
-    http.Route.BASE = os.environ.get("REST_PROXY")
-    print("Using proxy")
-else:
-    print("Not using proxy")
-
-bot = InteractionBot(sync_commands=False)
+bot = InteractionBot()
 
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}")
-    print(f"Gateway: {await bot.http.get_gateway()}")
-    print(f"Base: {http.Route.BASE}")
-
-
-@bot.slash_command(
-    name="github",
-)
-async def github(inter: CommandInteraction):
-    await inter.response.defer()
-    pass
 
 
 async def on_error(
-        inter: CommandInteraction,
-        exception: Exception,
+    inter: CommandInteraction,
+    exception: Exception,
 ):
     await inter.edit_original_message("something went wrong")
     traceback.print_exception(exception)
@@ -45,7 +27,4 @@ bot.on_slash_command_error = lambda inter, exception: on_error(inter, exception)
 
 bot.load_extension("gitget.repo")
 
-if proxy_on:
-    bot.run("evie")
-else:
-    bot.run(os.environ["DISCORD_TOKEN"])
+bot.run(os.environ["DISCORD_TOKEN"])
